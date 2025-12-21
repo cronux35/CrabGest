3878fe18d27299311476acb5bc703e8dc20625e0
François Legrée <francois.legree@gmail.com>
2025-12-21 18:36:58 +0100

    add firebase api key

--- scripts/db.js
+++ scripts/db.js
@@ -26,12 +26,13 @@ function initializeFirestore() {
     }
     if (!firebase.apps.length) {
         const firebaseConfig = {
-            apiKey: "VOTRE_API_KEY",
-            authDomain: "VOTRE_AUTH_DOMAIN",
-            projectId: "VOTRE_PROJECT_ID",
-            storageBucket: "VOTRE_STORAGE_BUCKET",
-            messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-            appId: "VOTRE_APP_ID"
+            apiKey: "AIzaSyC1ZP89QSoWubkcnMJJ6cinIAlFXXnTefU",
+            authDomain: "crabbrewgest.firebaseapp.com",
+            projectId: "crabbrewgest",
+            storageBucket: "crabbrewgest.firebasestorage.app",
+            messagingSenderId: "156226949050",
+            appId: "1:156226949050:web:52b3e666cc31e7963d5783",
+            measurementId: "G-MY8FH7L6K1"
         };
         firebase.initializeApp(firebaseConfig);
         firestoreDb = firebase.firestore();
